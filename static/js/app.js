648ea984ce02b718158eb4ac9f87d545e330a460
Kerigo Ndirangu <62352071+kgndirangu@users.noreply.github.com>
2023-12-24 22:31:30 -0500

    bubble chart

--- static/js/app.js
+++ static/js/app.js
@@ -31,7 +31,39 @@ dropdownMenu
    })  } )
   };
 
-//      
+
+  // Build bubble chart
+function buildBubble(sample) {
+  //D3 to fetch data
+d3.json(url).then(function(data) {
+  let samples = data.samples; 
+  let sampleArray = samples.filter(sampleDict => sampleDict.id == sample)
+});
+
+  //Grab the first index from array needed?
+let value = sampleArray[0];
+//value here is not a keyword, rather the name of each row of sample array
+//why don't we have to create a list and push each row to the list
+let otu_ids = value.otu_ids;
+let otu_labels = value.otu_labels;
+let sample_values = value.sample_values;
+console.log(otu_ids,otu_labels,sample_values);
+
+  let trace1 = {
+    x: otu_ids,
+    y: sample_values,
+    text: otu_labels,
+    mode: "markers",
+    marker: {
+        size: sample_values,
+        color: otu_ids,
+        colorscale: "Earth"
+    }
+      }};
+
+
+   
+    Plotly.newPlot("bubble", trace1);
 // //   //Initialize x and y arrays
 // //   let otu_names = []
 // //   let otu_numbers = []
@@ -45,18 +77,6 @@ dropdownMenu
 //   }
   
 //   };
-
-
-//   let trace = {
-//     x: otu_names,
-//     y: otu_numbers,
-//     type: "bar"
-//   };
-
-//   let otu = [trace];
-
-//   Plotly.newPlot("plot", otu);
-  
 // //   //Filter Day 2 Activity 4
 // //   //Slice data Day 2 Activity 9
 
